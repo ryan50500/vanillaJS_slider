396d5112acd0ac3628827475fad7a275587f2412
ryan woods <federer5050@gmail.com>
2020-11-02 17:31:03 +0000

    next slides function

--- main.js
+++ main.js
@@ -5,8 +5,20 @@ const buttons = document.querySelectorAll('.buttons div');
 function getNextPrev(){
     const activeSlide = document.querySelector('.slide.active');
     const activeIndex = slides.indexOf(activeSlide);
+    let next, prev;
 
-    console.log(activeIndex);
+    if(activeIndex === slides.length -1){
+        next = slides[0];
+    }else{
+        next = slides[activeIndex + 1];
+    } 
+ 
+    if(activeIndex === 0){
+        prev = slides[slides.length-1];
+    }else {
+        prev = slides[activeIndex - 1];
+    }
+    return [next, prev]
 }
 
 getNextPrev();
